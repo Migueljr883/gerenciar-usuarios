f969e3440f0b93aced18c874b6950893e6b47691
Migueljr883 <miguel22.adm@hotmail.com>
2025-09-14 19:46:39 -0300

    Mudei a lsita de nomes de usuários para tornarem-se objetos

--- src/gerenciarUsuarios.js
+++ src/gerenciarUsuarios.js
@@ -1,11 +1,36 @@
-const usuarios = ['Matheus', 'Marcos', 'Lucas', 'Joao'];
+/*
+    Exemplo:
+    {
+        primeiroPropriedade: 'Valor',
+        segundaPropriedade: 'Valor'
+    }
+*/
+
+const usuarios = [
+    {
+        nome: 'Matheus',
+        email: 'Matheus@gospel.com'
+    },
+    {
+        nome: 'Marcos',
+        email: 'Marcos@gospel.com'
+    },
+    {
+        nome: 'Lucas',
+        email: 'Lucas@gospel.com'
+    },
+    {
+        nome: 'Joao',
+        email: 'Joao@gospel.com'
+    }
+];
 
 function retornarUsuarios() {
     return usuarios;
 }
 
-function adicionarNovoUsuario(nome) {
-    usuarios.push(nome);
+function adicionarNovoUsuario(usuario) {
+    usuarios.push(usuario);
 }
 
 module.exports = {
--- test/gerenciarUsuarios.test.js
+++ test/gerenciarUsuarios.test.js
@@ -4,12 +4,21 @@ const { expect } = require('chai');
 describe('Testar as funçoes de Gestao de usuarios', function () {
     it('Validar que posso adicionar um novo nome de usuario na lista', function() {
         //1.Adicionar um novo nome na lista de usuarios
-        adicionarNovoUsuario('Maria');
+        adicionarNovoUsuario({
+            nome: 'Maria',
+            email: 'maria@gospel.com'
+        });
 
         //2.retornar a lista de usuariosna caixa 
         const listaDeUsuarios = retornarUsuarios();
 
         //3. comparar se o novo nome esta no fim da lista de usuarios
-        expect(listaDeUsuarios.at(-1)).to.equal('Maria');
-    });
+        //expect(listaDeUsuarios.at(-1).nome).to.equal('Maria');
+        //expect(listaDeUsuarios.at(-1).email).to.equal('maria@gospel.com');
+        expect(listaDeUsuarios.at(-1)).to.eql({ //to.deep.equal
+            nome: 'Maria',
+            email: 'maria@gospel.com'
+         });
+
+    })
 });
